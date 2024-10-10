b94fe266f7258ce37b469b5a2fe09007104f59f2
Nguyen Danh Khanh <nguyendanhkhanh12a2k44dongloc@gmail.com>
2024-10-10 22:43:10 +0700

    config mails

--- src/main/java/com/bezkoder/springjwt/config/MailConfig.java
+++ src/main/java/com/bezkoder/springjwt/config/MailConfig.java
@@ -22,8 +22,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("khanhson26122001@gmail.com");
-        mailSender.setPassword("hxkfggcrpdfkubbo");
+        mailSender.setUsername("tonthai8916@gmail.com");
+        mailSender.setPassword("ugmijlagqnzixcxq");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
